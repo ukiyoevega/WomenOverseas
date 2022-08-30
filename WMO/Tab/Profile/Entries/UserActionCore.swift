ed091cf831a313f8c4bffdcd07b6756be48237c8
Jia Wei <jiajia.gulu@gmail.com>
2022-08-30 13:35:01 +0800

    fix(build): Xcode13.4.1 build error

--- WMO/Tab/Profile/Entries/UserActionCore.swift
+++ WMO/Tab/Profile/Entries/UserActionCore.swift
@@ -6,6 +6,7 @@
 //
 
 import ComposableArchitecture
+import Foundation
 
 struct UserActionState: Equatable {
     enum `Type`: Int {
--- WMO/Tab/TopicList/TopicListCore.swift
+++ WMO/Tab/TopicList/TopicListCore.swift
@@ -7,6 +7,7 @@
 
 import ComposableArchitecture
 import Combine
+import Foundation
 
 // MARK: - Topic
 
